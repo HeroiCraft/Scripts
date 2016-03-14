60fd8b1ce729646fab061e718e56500e3c81e252
HeroCC <HeroCC@herocc.com>
2016-03-14 18:37:03 -0400

    If .noStart exists, don't start server

--- run.sh
+++ run.sh
@@ -6,21 +6,22 @@ no_window=true
 cd $HeroiCraftDIR
 
 function mc_server {
-	cd $HeroiCraftDIR
-	echo "Waiting for Minecraft: $1 to start..."
-	bgsc mc_$1
-	sleep .1
-	screen -S mc_$1 -X stuff " ./startserver.sh $1 \n"
-	sleep .1
-  if [[ "$no_window" != true ]]; then
-    gnome-terminal --command="screen -r mc_$1"
-  fi
-  cd $HeroiCraftDIR
-  sleep 5s
+cd $HeroiCraftDIR
+echo "Waiting for Minecraft: $1 to start..."
+bgsc mc_$1
+sleep .1
+screen -S mc_$1 -X stuff " ./startserver.sh $1 \n"
+sleep .1
+if [[ "$no_window" != true ]]; then
+  gnome-terminal --command="screen -r mc_$1"
+fi
+cd $HeroiCraftDIR
+sleep 5s
 }
 
 function main {
-  if [[ -z "$1" ]]; then
+if [[ -z "$1" ]]; then
+  if [[ ! -a "$HOME/.noStart" ]]; then
     mc_server bungee
 
     mc_server hub
@@ -33,8 +34,11 @@ function main {
 
     mc_server games
   else
-    mc_server "$1"
+    echo "Not Starting, .noStart exists"
   fi
+else
+  mc_server "$1"
+fi
 }
 
 main "$@"
